aabb003f6dfee86dcbc58711853707cb77a702ef
Shelly Branch <sbranch@homeaway.com>
2019-01-23 20:53:28 +0000

    The date has been reformated to show the actual date and not just a long number.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,74 +1,57 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+/*  Client-side JS logic goes here jQuery is already loaded Reminder: Use (and d
+ * o all your DOM work in) jQuery's document ready function
+
  */
 
+$(document).ready(function () {
+    function renderTweets(tweets) {
+        tweets.forEach(function (tweet) {
+            let element = createTweetElement(tweet);
+            $("#tweet-container").append(element);
+        });
+    }
+    function createTweetElement(tweet) {
+        var avatars = tweet["user"].avatars.small;
+        // Header content
+        var img = $('<img>').attr({src: avatars});
+        var h3 = $('<h3>').text(tweet["user"].name);
+        var span = $('<span>').text(tweet["user"].handle);
+        // Appending all to header
+        var header = $('<header>')
+            .append(h3)
+            .append(img)
+            .append(span);
+        // End Header content Tweet body
+        var article = $('<article>').text(tweet["content"].text);
+        // End Tweet body Footer content
+        var parsed = parseInt(tweet.created_at);
+        var event = new Date(parsed);
+        let newDate = event.toDateString();
 
-// vars for the data
+        console.log(newDate);
+        //console.log(parsed);
+        var postDate = $('<span>').text(newDate);
 
-//   var created = data.created_at;
-//   var name = data["user"].name;
-//   var handle = data["user"].handle;
-//   var avatars = data["user"].avatars.small;
+        var footer = $('<footer>').append(postDate);
+        //console.log(text)
+        let $tweet = $('<article>')
+            .addClass('tweets')
+            .append(header)
+            .append(article)
+            .append(footer);
 
-$(document).ready(function() {
+        return $tweet;
+    }
 
-  function renderTweets(tweets) {
-    // loops through tweets
-      // calls createTweetElement for each tweet
-      // takes return value and appends it to the tweets container
-      tweets.forEach(function(tweet) {
-        //console.log(tweet)
-        let element = createTweetElement(tweet);
-        //console.log(element)
-        $("#tweet-container").append(element);
-       // console.log($("#tweet-container"))
-      });
-      
- 
-  }
+    function loadTweets() {
+        $
+            .ajax('/tweets', {method: 'GET'})
+            .then(function (bigTweets) {
+                //console.log('Success: ', bigTweets);
+                renderTweets(bigTweets);
+            });
 
-
-
-  
-  function createTweetElement(tweet) {
-
-    var avatars = tweet["user"].avatars.small;
-    
-// Header content   
-    var img = $('<img>').attr({ src: avatars});
-    var h3 = $('<h3>').text(tweet["user"].name);
-    var span = $('<span>').text(tweet["user"].handle);
-// Appending all to header
-    var header = $('<header>').append(h3).append(img).append(span);
-// End Header content 
-
-
-// Tweet body
-   var article = $('<article>').text(tweet["content"].text);
-// End Tweet body
-
-// Footer content
-   var postDate = $('<span>').text(tweet.created_at);
-   
-   var footer =$('<footer>').append(postDate);
-//console.log(text)
-    let $tweet = $('<article>').addClass('tweets').append(header).append(article).append(footer);
-    
-    return $tweet;
-  }
-  
-
-  function loadTweets(){
-    $.ajax('/tweets', { method: 'GET' })
-    .then(function (bigTweets) {
-      console.log('Success: ', bigTweets);
-      renderTweets(bigTweets);
-    });
-  
-
-  }
-  loadTweets();
+    }
+    loadTweets();
 
 });
